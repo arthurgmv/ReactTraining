a7457b21ec9697e6674bbea1d3ad83bd197bc346
Arthur Gabriel <123274385+arthurgmv@users.noreply.github.com>
2023-08-15 17:10:15 -0300

    Initial commit

---
+++ src/List.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function List() {
+  return (
+    <div>
+      <ul>
+        <li>Bacon</li>
+        <li>Burgers</li>
+        <li>Pizza</li>
+      </ul>
+    </div>
+  );
+}
+
+export default List;
